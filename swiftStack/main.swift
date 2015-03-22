291282307570f3d0065139a345c72f4c98450225
Matthew Chupp <mattlchupp@gmail.com>
2015-03-21 22:23:13 -0500

    Initial Commit

---
+++ swiftStack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftStack
+//
+//  Created by Matthew Chupp on 3/21/15.
+//  Copyright (c) 2015 MattChupp. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
